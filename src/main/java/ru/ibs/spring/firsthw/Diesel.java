455de4b761ef03870694b676f5284c4141886fe8
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-11-07 23:00:20 +0300

    Исправил,убрал лишний класс.

--- src/main/java/ru/ibs/spring/firsthw/Diesel.java
+++ src/main/java/ru/ibs/spring/firsthw/Diesel.java
@@ -1,6 +1,7 @@
 package ru.ibs.spring.firsthw;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -8,11 +9,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Diesel implements Engine{
-  /*  private EngineImpl engineImpl;
-    Diesel(EngineImpl engineImpl){
-        this.engineImpl = engineImpl;
-    }*/
+    @Autowired
+    private Engine engine;
 
+    private Diesel diesel;
+
+
+    @Override
+    public void powerUpSecondVersion() {
+        System.out.println(engine.getClass()+ " Работает на дизеле." );
+    }
+
+    //пока что не нужно.
     @Override
     public String powerUp() {
       return  "Работает на дизеле.";
