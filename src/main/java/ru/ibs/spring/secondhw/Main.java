eb06cfe73d11f621d6b8c0537b83ea9c8d7e1b09
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-11-07 23:21:12 +0300

    изменения

--- src/main/java/ru/ibs/spring/secondhw/Main.java
+++ src/main/java/ru/ibs/spring/secondhw/Main.java
@@ -2,13 +2,18 @@ package ru.ibs.spring.secondhw;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.ibs.spring.firsthw.Diesel;
+import ru.ibs.spring.firsthw.Engine;
+import ru.ibs.spring.firsthw.Petrol;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext("ru.ibs.spring.secondhw");
-        EngineImplements engineImplements = context.getBean("engineImplements",EngineImplements.class);
-        engineImplements.showEng();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("ru.ibs.spring.secondhw");
 
 
+        ru.ibs.spring.firsthw.Engine engineDiesel = applicationContext.getBean("diesel", Diesel.class);
+        engineDiesel.powerUpSecondVersion();
+        Engine enginePetrol = applicationContext.getBean("petrol", Petrol.class);
+        enginePetrol.powerUpSecondVersion();
     }
 }
