dbf65092d60ad2de7f9f2350ade2290db641c1f3
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-11-08 03:38:00 +0300

    3:50 .Сервер не запустился еще

---
+++ src/main/java/ru/ibs/spring/secondhw/AspException.java
@@ -0,0 +1,4 @@
+package ru.ibs.spring.secondhw;
+
+public class Example {
+}
---
+++ src/main/java/ru/ibs/spring/secondhw/MyController.java
@@ -0,0 +1,18 @@
+package ru.ibs.spring.mvc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/mvc")
+public class MyController {
+
+    @GetMapping("example")
+    public String getExample(){
+        return "example";
+    }
+}
+
